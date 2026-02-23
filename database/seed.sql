d8e9455ca5a4fa74a8f369502098312cffa648f1
Ranskyth <ga.lima2205@gmail.com>
2026-02-22 23:11:54 -0300

    feat: create seed

---
+++ database/seed.sql
@@ -0,0 +1,15 @@
+insert into users values 
+(default, "marcos", "11 111111111", "user@gmail.com", "123456789"),
+(default, "pedro", "11 111111111", "user2@gmail.com", "123456789"),
+(default, "mariano", "11 111111111", "user3@gmail.com", "123456789")
+
+INSERT INTO produtos (id, descricao, nome, valorCentavos, imagem) VALUES
+(default, default, 'Shampoo Ice para Barba e Cabelo - 220ml - Premium - Barba de Respeito', 5990, 'Shampoo.png.webp'),
+(default, default, 'Shampoo Para Barba e Cabelo Refrescante - 1L - Barba de Respeito', 15990, 'Shampoo1litro.png.webp'),
+(default, default, 'Shampoo de Barba 200ml - Linha Essencial - Barba de Respeito', 3490, 'shampoebarba.webp'),
+(default, default, 'Pó Modelador Fixação Extrema - Premium - Barba de Respeito', 7990, 'Pomodelador.png.webp'),
+(default, default, 'Blend Capilar - Tônico pró Crescimento + Shampoo Antiqueda - Barba de Respeito', 9990, 'Anticalvice.png.webp'),
+(default, default, 'Cera Modeladora Cabelos Efeito Molhado - 80g - Premium - Barba de Respeito', 6990, 'Cera.png.webp'),
+(default, default, 'Escurecedor Barba e Cabelo - Premium - Barba de Respeito', 8990, 'Escurecedor.png.webp'),
+(default, default, 'Blend Original® para Crescimento de Barba - 1 Mês de Tratamento - Barba de Respeito', 7890, 'blend_e_shampoo_ice.webp'),
+(default, default, 'Blend Original 30 ml + Shampoo Refrescante Para Barba Ice 220 ml', 10990, 'blend_barba.webp');
